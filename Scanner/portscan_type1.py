d6acd418ada85580810b320f24809fd99b7af3d9
Piyush Acharya <coveringguy0123@gmail.com>
2022-03-15 00:18:31 +0545

    Completely Basic Port Scanner that scans port on a single PORT

---
+++ Scanner/portscan_type1.py
@@ -0,0 +1,16 @@
+# AUTHOR: Piyush Acharya(~r3alix01)
+
+import socket
+
+socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.AF_INET for ipv4 and socket.SOCK_STREAM for TCP
+host = input("[+] Enter the HOST to scan: ")
+port = int(input(("[+] Enter the PORT to scan: ")))
+
+def portscan(port):
+	'''Checks if connection receives to the port receives an error'''
+	if socks.connect_ex((host, port)):
+		print(f"port {port} is closed")
+	else:
+		print(f"port {port} is open")
+
+portscan(port) 
