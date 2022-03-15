c20a53d0b905436a199f015fd2b43b75740565a6
Piyush Acharya <coveringguy0123@gmail.com>
2022-03-15 12:09:17 +0545

    Added a color feature and user specififed port to scan

--- Scanner/portscan_type1.py
+++ Scanner/portscan_type1.py
@@ -1,17 +1,21 @@
 # AUTHOR: Piyush Acharya(~r3alix01)
 
 import socket
+from termcolor import colored
 
 socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.AF_INET for ipv4 and socket.SOCK_STREAM for TCP
-socket.setdefaulttimeout(5) # Add a timeout to 5 seconds
+socket.setdefaulttimeout(3) # Add a timeout to 3 seconds
 host = input("[+] Enter the HOST to scan: ")
-port = int(input(("[+] Enter the PORT to scan: ")))
+lower_range = int(input("[+] Enter the lower range port: "))
+upper_range = int(input("[+] Enter the upper range port: "))
+# port = int(input(("[+] Enter the PORT to scan: ")))
 
 def portscan(port):
 	'''Checks if connection receives to the port receives an error'''
 	if socks.connect_ex((host, port)):
-		print(f"port {port} is closed")
+		print(colored(f'[!]Port {port} is closed', 'red'))
 	else:
-		print(f"port {port} is open")
+		print(colored(f'[!]Port {port} is open', 'green'))
 
-portscan(port) 
+for port in range(lower_range, upper_range+1): # Scans the specified ports
+	portscan(port) 
