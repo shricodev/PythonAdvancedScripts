e5b5aa60af11d66f19d3285315902c8911f566d7
Piyush Acharya <user@example.com>
2022-03-20 16:02:14 +0545

    Uses SeclistsWordlists, Takes the hash and get hash for every element in list and compare the one which matches is the  decoded value

---
+++ PasswdCrack/sha1decoder.py
@@ -0,0 +1,18 @@
+#AUTHOR: Piyush Aacharya (r3alix01)
+
+import hashlib
+from requests import get
+
+sha1hash = input("[+] Enter the SHA1 hash to decode: ")
+url_content = get("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt", ).text.split("\n")
+
+for item in url_content:
+    guess = hashlib.sha1(bytes(item, 'utf-8')).hexdigest()
+    # print(guess)
+    if guess == sha1hash:
+        print(f"[+]Decoded SHA1 hash is : {item}")
+        exit(1)
+    else:
+        print(f"[-] Password guess '{item}' doesnot match, going for next...")
+
+print("[-] Password Not in The password list")
